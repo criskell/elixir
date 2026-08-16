6605f8d47f323d4c26c656d5bc19f51b937130ad
criskell <cris.moraes.ds@gmail.com>
2026-08-16 06:45:48 -0300

    chore: init library package

---
+++ lib/rewritetoday.ex
@@ -0,0 +1,18 @@
+defmodule Rewritetoday do
+  @moduledoc """
+  Documentation for `Rewritetoday`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rewritetoday.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rewritetoday_test.exs
@@ -0,0 +1,8 @@
+defmodule RewritetodayTest do
+  use ExUnit.Case
+  doctest Rewritetoday
+
+  test "greets the world" do
+    assert Rewritetoday.hello() == :world
+  end
+end
